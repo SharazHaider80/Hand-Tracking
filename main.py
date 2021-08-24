f5ebac6685320a9969d91685f1ab0919ad7c43a1
SharazHaider80 <55837058+SharazHaider80@users.noreply.github.com>
2021-08-24 12:54:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import cv2
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
+mp_hands = mp.solutions.hands
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
+with mp_hands.Hands(
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
+    min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -22,17 +23,20 @@ with mp_pose.Pose(
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
-    results = pose.process(image)
+    results = hands.process(image)
 
-    # Draw the pose annotation on the image.
+    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    cv2.imshow('MediaPipe Pose', image)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
